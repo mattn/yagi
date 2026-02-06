0d4fb381179ad63b1c6dd00a87234fba0921818d
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-02-06 13:53:19 +0900

    Replace go-nostr with generic FetchURL and WebSocketSend host APIs
    - FetchURL: fetch URL content, convert HTML to text with links preserved,
      reject binary content types
    - WebSocketSend: generic WebSocket client using gorilla/websocket
    - Remove go-nostr dependency

--- hostapi.go
+++ hostapi.go
@@ -1,69 +1,129 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"strings"
 	"time"
 
-	"github.com/nbd-wtf/go-nostr"
+	"github.com/gorilla/websocket"
+	"golang.org/x/net/html"
 )
 
-func nostrFetchNotes(relay string, limit int) string {
-	if limit <= 0 {
-		limit = 10
-	}
-	if limit > 50 {
-		limit = 50
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	r, err := nostr.RelayConnect(ctx, relay)
+func fetchURL(url string) string {
+	resp, err := http.Get(url)
 	if err != nil {
-		return fmt.Sprintf("Error connecting to relay: %v", err)
+		return fmt.Sprintf("Error: %v", err)
 	}
-	defer r.Close()
+	defer resp.Body.Close()
 
-	filters := nostr.Filters{{
-		Kinds: []int{nostr.KindTextNote},
-		Limit: limit,
-	}}
-
-	sub, err := r.Subscribe(ctx, filters)
-	if err != nil {
-		return fmt.Sprintf("Error subscribing: %v", err)
+	ct := resp.Header.Get("Content-Type")
+	if !strings.Contains(ct, "text/") && !strings.Contains(ct, "application/json") && !strings.Contains(ct, "application/xml") {
+		return fmt.Sprintf("Error: unsupported content type: %s", ct)
 	}
-	defer sub.Unsub()
 
-	var events []*nostr.Event
-	for {
-		select {
-		case ev, ok := <-sub.Events:
-			if !ok {
-				goto done
-			}
-			events = append(events, ev)
-		case <-sub.EndOfStoredEvents:
-			goto done
-		case <-ctx.Done():
-			goto done
+	if !strings.Contains(ct, "text/html") {
+		b, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return fmt.Sprintf("Error: %v", err)
 		}
+		return string(b)
 	}
-done:
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return fmt.Sprintf("Error parsing HTML: %v", err)
+	}
 
 	var sb strings.Builder
-	for i, ev := range events {
-		t := ev.CreatedAt.Time().Format("2006-01-02 15:04:05")
-		content := ev.Content
-		if len(content) > 200 {
-			content = content[:200] + "..."
-		}
-		fmt.Fprintf(&sb, "[%d] %s (by %s...)\n%s\n\n", i+1, t, ev.PubKey[:12], content)
-	}
-	if sb.Len() == 0 {
-		return "No events found."
-	}
+	extractText(doc, &sb)
 	return sb.String()
+}
+
+func extractText(n *html.Node, sb *strings.Builder) {
+	switch n.Type {
+	case html.ElementNode:
+		switch n.Data {
+		case "script", "style", "noscript":
+			return
+		case "br":
+			sb.WriteString("\n")
+		case "p", "div", "h1", "h2", "h3", "h4", "h5", "h6", "li", "tr", "blockquote":
+			sb.WriteString("\n")
+		}
+
+		var href string
+		if n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					href = a.Val
+					break
+				}
+			}
+		}
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			extractText(c, sb)
+		}
+
+		if href != "" {
+			sb.WriteString(" (")
+			sb.WriteString(href)
+			sb.WriteString(")")
+		}
+
+		switch n.Data {
+		case "p", "div", "h1", "h2", "h3", "h4", "h5", "h6", "li", "tr", "blockquote":
+			sb.WriteString("\n")
+		}
+	case html.TextNode:
+		text := strings.TrimSpace(n.Data)
+		if text != "" {
+			sb.WriteString(text)
+			sb.WriteString(" ")
+		}
+	default:
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			extractText(c, sb)
+		}
+	}
+}
+
+func webSocketSend(url string, message string, maxMessages int, timeoutSec int) string {
+	if maxMessages <= 0 {
+		maxMessages = 10
+	}
+	if timeoutSec <= 0 {
+		timeoutSec = 10
+	}
+
+	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		return fmt.Sprintf("Error connecting: %v", err)
+	}
+	defer conn.Close()
+
+	if err := conn.WriteMessage(websocket.TextMessage, []byte(message)); err != nil {
+		return fmt.Sprintf("Error sending: %v", err)
+	}
+
+	deadline := time.Now().Add(time.Duration(timeoutSec) * time.Second)
+	conn.SetReadDeadline(deadline)
+
+	var results []string
+	for i := 0; i < maxMessages; i++ {
+		_, data, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		results = append(results, string(data))
+	}
+
+	conn.WriteMessage(websocket.CloseMessage,
+		websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+
+	b, _ := json.Marshal(results)
+	return string(b)
 }
