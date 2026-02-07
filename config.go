e279bbfe8d4027c44aab92759115b15e1a56a448
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-02-07 14:13:05 +0900

    Add missing file

---
+++ config.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+type Config struct {
+	Prompt string `json:"prompt"`
+}
+
+var appConfig = Config{
+	Prompt: "🐐 ",
+}
+
+func loadConfig(configDir string) error {
+	path := filepath.Join(configDir, "config.json")
+	data, err := os.ReadFile(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return err
+	}
+	return json.Unmarshal(data, &appConfig)
+}
