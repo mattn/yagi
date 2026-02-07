7e15c8b18c7fbbd9f1e09807d4934c34025d1a13
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-02-08 02:22:07 +0900

    Bump up version to 0.0.17

--- main.go
+++ main.go
@@ -159,7 +159,7 @@ func chat(ctx context.Context, client *openai.Client, messages []openai.ChatComp
 
 const name = "yagi"
 
-const version = "0.0.16"
+const version = "0.0.17"
 
 var revision = "HEAD"
 
