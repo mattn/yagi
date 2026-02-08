2735094d7b28a50090522006925125d929ea5ecb
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-02-09 01:56:54 +0900

    Implement slash commands

--- readline.go
+++ readline.go
@@ -1,20 +1,48 @@
 package main
 
 import (
+	"bufio"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/chzyer/readline"
 )
 
 var rl *readline.Instance
 
+func modelCompleter() []string {
+	var models []string
+	scanner := bufio.NewScanner(strings.NewReader(modelsTxt))
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line != "" {
+			models = append(models, line)
+		}
+	}
+	return models
+}
+
 func initReadline(prompt, configDir string) error {
+	models := modelCompleter()
+	var modelItems []readline.PrefixCompleterInterface
+	for _, m := range models {
+		modelItems = append(modelItems, readline.PcItem(m))
+	}
+
 	cfg := &readline.Config{
 		Prompt:          prompt,
 		InterruptPrompt: "^C",
 		EOFPrompt:       "exit",
 		Stderr:          os.Stderr,
+		AutoComplete: readline.NewPrefixCompleter(
+			readline.PcItem("/help"),
+			readline.PcItem("/model", modelItems...),
+			readline.PcItem("/clear"),
+			readline.PcItem("/save"),
+			readline.PcItem("/memory"),
+			readline.PcItem("/session"),
+		),
 	}
 	if configDir != "" {
 		cfg.HistoryFile = filepath.Join(configDir, "history")
