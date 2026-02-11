1ba204333aae04debce602d013ef5b18dea704dc
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-02-11 23:37:28 +0900

    Open CONIN$, CONOUT$ for read/write

--- tty_windows.go
+++ tty_windows.go
@@ -5,21 +5,30 @@ package main
 import (
 	"bufio"
 	"os"
+	"syscall"
 )
 
 func readFromTTY(prompt string) (string, error) {
-	tty, err := os.OpenFile("CON", os.O_RDWR, 0)
+	inHandle, err := syscall.Open("CONIN$", syscall.O_RDWR, 0)
 	if err != nil {
 		return "", err
 	}
-	defer tty.Close()
+	ttyIn := os.NewFile(uintptr(inHandle), "CONIN$")
+	defer ttyIn.Close()
 
-	_, err = tty.WriteString(prompt)
+	outHandle, err := syscall.Open("CONOUT$", syscall.O_RDWR, 0)
+	if err != nil {
+		return "", err
+	}
+	ttyOut := os.NewFile(uintptr(outHandle), "CONOUT$")
+	defer ttyOut.Close()
+
+	_, err = ttyOut.WriteString(prompt)
 	if err != nil {
 		return "", err
 	}
 
-	reader := bufio.NewReader(tty)
+	reader := bufio.NewReader(ttyIn)
 	response, err := reader.ReadString('\n')
 	if err != nil {
 		return "", err
